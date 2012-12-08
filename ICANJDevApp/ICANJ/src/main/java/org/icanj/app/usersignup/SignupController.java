69a8904c4f214406ca58479256dab838a4642703
robinvk6 <robinvk6@gmail.com>
2012-12-07 21:29:55 -0500

    Fixed an error in SignupController

--- ICANJDevApp/ICANJ/src/main/java/org/icanj/app/usersignup/SignupController.java
+++ ICANJDevApp/ICANJ/src/main/java/org/icanj/app/usersignup/SignupController.java
@@ -5,7 +5,6 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.icanj.app.directory.service.DirectoryService;
 import org.springframework.beans.factory.annotation.Autowired;
->>>>>>> Initial code push.
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
